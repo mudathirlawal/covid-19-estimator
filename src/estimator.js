6cc19f928112f9094c5dae1cb5f93dc7ddddb3fb
Mudathir Lawal <nigercode@gmail.com>
2020-04-13 17:49:37 +0100

    set up the function with the given data and outpu format

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,31 @@
-const covid19ImpactEstimator = (data) => data;
+
+let data = {
+    region: {
+        name: "Africa",
+        avgAge: 19.7,
+        avgDailyIncomeInUSD: 5,
+        avgDailyIncomePopulation: 0.71
+    },
+    periodType: "days",
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
+};
+   
+// Here I would have employed the IIFE Pattern and returned 
+// my output as a closure so my functions can be reused; but
+// but I will stick with the pattern you've layed out.
+const covid19ImpactEstimator = ( data ) => {
+
+    //  My computations go here:
+    
+
+    return {
+        data: data, 
+        impact: impact, 
+        severeImpact: severeImpact 
+    };      
+};
 
 export default covid19ImpactEstimator;
