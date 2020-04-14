8ee28ef994aebfc5a98fa11414aa0496e870ea73
Mudathir Lawal <nigercode@gmail.com>
2020-04-14 23:32:18 +0100

    Solution to question 1

--- src/estimator.js
+++ src/estimator.js
@@ -12,20 +12,59 @@ let data = {
     population: 66622705,
     totalHospitalBeds: 1380614
 };
-   
-// Here I would have employed the IIFE Pattern and returned 
-// my output as a closure so my functions can be reused; but
-// but I will stick with the pattern you've layed out.
-const covid19ImpactEstimator = ( data ) => {
 
-    //  My computations go here:
+let { 
+    region, 
+    periodType, 
+    timeToElapse, 
+    reportedCases, 
+    population, 
+    totalHospitalBeds 
+} = data;
+
+let covid19ImpactEstimator = (
+    ( data ) => {
+        const NO_OF_DAYS_IN_WEEK = 7;
+        const NO_OF_DAYS_IN_MONTH = 30;
+        const DOUBLING_INTERVAL = 3;
+        let factor;
     
-
-    return {
-        data: data, 
-        impact: impact, 
-        severeImpact: severeImpact 
-    };      
-};
+        const getFactor = _ => { 
+            if ( periodType === "days" ) {
+                return factor = Math.floor( timeToElapse / DOUBLING_INTERVAL );
+            } else if ( periodType === "weeks" ) {
+                timeToElapse = timeToElapse / NO_OF_DAYS_IN_WEEK;
+                return factor = Math.floor( timeToElapse / DOUBLING_INTERVAL );
+            } else if ( periodType === "months" ) {
+                timeToElapse = timeToElapse / NO_OF_DAYS_IN_MONTH;
+                return factor = Math.floor( timeToElapse / DOUBLING_INTERVAL );
+            }
+        };
+            
+        const impact = _ =>  {
+            factor = getFactor();
+            const currentlyInfected = reportedCases * 10;
+            return { 
+                currentlyInfected: currentlyInfected,
+                infectionsByRequestedTime: currentlyInfected * ( 2 ** factor )
+            };
+        };
+        const severeImpact = _ => {
+            factor = getFactor(); 
+            const currentlyInfected = reportedCases * 50;
+            return {
+                currentlyInfected: currentlyInfected,
+                infectionsByRequestedTime: currentlyInfected * ( 2 ** factor )
+            };
+        };
+    
+        let estimation = {
+            data: data, 
+            impact: impact, 
+            severeImpact: severeImpact
+        };  
+        return estimation;    
+    }
+)();
 
 export default covid19ImpactEstimator;
